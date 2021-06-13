9b3ebf76db6a71509a1d3e08fe6092f88fa9fb3a
johnson <you@example.com>
2021-06-13 22:48:08 +0800

    dbutils

---
+++ celery_demo/flower_demo.py
@@ -0,0 +1,4 @@
+# flower -A tasks --port 9999
+# pip3 install flower
+# monitor removed on purpose for integration with prometheus
+# https://github.com/mher/flower/issues/895#issuecomment-786253876
